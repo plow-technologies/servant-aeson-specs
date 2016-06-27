b966624ec829c4d414d4202d69c99269886e1796
Sönke Hahn <soenkehahn@gmail.com>
2016-06-27 17:48:34 +0800

    refactoring: removed some type-level magic

--- src/Servant/Aeson/RoundtripSpecs.hs
+++ src/Servant/Aeson/RoundtripSpecs.hs
@@ -7,9 +7,9 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 module Servant.Aeson.RoundtripSpecs (
-  allRoundtripSpec,
+  roundtripSpecs,
+  HasRoundtripSpecs(..),
   usedTypes,
-  HasUsedTypes(..),
 
   -- * re-exports
   Proxy(..),
@@ -25,52 +25,45 @@ import           Test.QuickCheck
 
 import           Test.Aeson.RoundtripSpecs
 
-allRoundtripSpec :: (HasRoundtripSpecs (UsedTypes api), HasUsedTypes api) =>
-  Proxy api -> Spec
-allRoundtripSpec = roundtripSpecs . usedTypes
+roundtripSpecs :: (HasRoundtripSpecs api) => Proxy api -> Spec
+roundtripSpecs = sequence_ . map snd . mkRoundtripSpecs
 
-class HasRoundtripSpecs (types :: [*]) where
-  roundtripSpecs :: Proxy types -> Spec
+usedTypes :: (HasRoundtripSpecs api) => Proxy api -> [TypeRep]
+usedTypes = map fst . mkRoundtripSpecs
 
-instance forall a r .
-  (Typeable a, Eq a, Show a, Arbitrary a, ToJSON a, FromJSON a, HasRoundtripSpecs r) =>
-  HasRoundtripSpecs (a ': r) where
+mkSpec :: (Typeable a, Eq a, Show a, Arbitrary a, ToJSON a, FromJSON a) =>
+  Proxy a -> [(TypeRep, Spec)]
+mkSpec proxy =
+  [(typeRep proxy, genericAesonRoundtrip proxy)]
 
-  roundtripSpecs Proxy = do
-    genericAesonRoundtrip (Proxy :: Proxy a)
-    roundtripSpecs (Proxy :: Proxy r)
+class HasRoundtripSpecs api where
+  mkRoundtripSpecs :: Proxy api -> [(TypeRep, Spec)]
 
-instance HasRoundtripSpecs '[] where
-  roundtripSpecs Proxy = return ()
+instance (HasRoundtripSpecs a, HasRoundtripSpecs b) => HasRoundtripSpecs (a :<|> b) where
+  mkRoundtripSpecs p =
+    mkRoundtripSpecs (Proxy :: Proxy a) ++
+    mkRoundtripSpecs (Proxy :: Proxy b)
 
--- * UsedTypes
+instance (Eq response, Show response, Typeable response, Arbitrary response, ToJSON response, FromJSON response) =>
+  HasRoundtripSpecs (Get contentTypes response) where
 
-usedTypes :: Proxy api -> Proxy (UsedTypes api)
-usedTypes Proxy = Proxy
+  mkRoundtripSpecs Proxy = do
+    mkSpec (Proxy :: Proxy response)
 
-class HasUsedTypes api where
-  type UsedTypes api :: [*]
+instance (Eq response, Show response, Typeable response, Arbitrary response, ToJSON response, FromJSON response) =>
+  HasRoundtripSpecs (Post contentTypes response) where
 
-instance HasUsedTypes (a :<|> b) where
-  type UsedTypes (a :<|> b) = UsedTypes a ++ UsedTypes b
+  mkRoundtripSpecs Proxy = mkSpec (Proxy :: Proxy response)
 
-instance HasUsedTypes (Get contentTypes response) where
-  type UsedTypes (Get contentTypes response) = '[response]
+instance (Eq body, Show body, Typeable body, Arbitrary body, ToJSON body, FromJSON body, HasRoundtripSpecs api) =>
+  HasRoundtripSpecs (ReqBody contentTypes body :> api) where
 
-instance HasUsedTypes (Post contentTypes response) where
-  type UsedTypes (Post contentTypes response) = '[response]
+  mkRoundtripSpecs Proxy =
+    mkSpec (Proxy :: Proxy body) ++
+    mkRoundtripSpecs (Proxy :: Proxy api)
 
-instance HasUsedTypes ((path :: Symbol) :> api) where
-  type UsedTypes (path :> api) = UsedTypes api
+instance HasRoundtripSpecs api => HasRoundtripSpecs ((path :: Symbol) :> api) where
+  mkRoundtripSpecs Proxy = mkRoundtripSpecs (Proxy :: Proxy api)
 
-instance HasUsedTypes (ReqBody contentTypes body :> api) where
-  type UsedTypes (ReqBody contentTypes body :> api) = body ': UsedTypes api
-
-instance HasUsedTypes (MatrixParam name a :> api) where
-  type UsedTypes (MatrixParam name a :> api) = UsedTypes api
-
--- * type level (++)
-
-type family (a :: [*]) ++ (b :: [*]) where
-  '[] ++ b = b
-  (a ': r) ++ b = a ': (r ++ b)
+instance HasRoundtripSpecs api => HasRoundtripSpecs (MatrixParam name a :> api) where
+  mkRoundtripSpecs Proxy = mkRoundtripSpecs (Proxy :: Proxy api)
--- test/Servant/Aeson/RoundtripSpecsSpec.hs
+++ test/Servant/Aeson/RoundtripSpecsSpec.hs
@@ -3,6 +3,7 @@
 
 module Servant.Aeson.RoundtripSpecsSpec where
 
+import           Data.Typeable
 import           Servant.API
 import           Test.Hspec
 import           Test.Hspec.Core.Runner
@@ -12,32 +13,32 @@ import           Test.Aeson.RoundtripSpecsSpec
 
 spec :: Spec
 spec = do
-  describe "allRoundtripSpec" $ do
+  describe "roundtripSpecs" $ do
     it "detects failures in types from ReqBody" $ do
-      allRoundtripSpec reqBodyFailApi `shouldTestAs`
+      roundtripSpecs reqBodyFailApi `shouldTestAs`
         Summary 2 1
 
     it "detects failures in types from Get" $ do
-      allRoundtripSpec getFailApi `shouldTestAs`
+      roundtripSpecs getFailApi `shouldTestAs`
         Summary 1 1
 
   describe "usedTypes" $ do
     it "extracts types from ReqBody" $ do
       usedTypes reqBodyFailApi `shouldBe`
-        (Proxy :: Proxy '[FaultyRoundtrip, Bool])
+        [faultyRoundtripRep, boolRep]
 
     it "extracts types from Get" $ do
       usedTypes getFailApi `shouldBe`
-        (Proxy :: Proxy '[FaultyRoundtrip])
+        [faultyRoundtripRep]
 
     it "traverses :>" $ do
       usedTypes (Proxy :: Proxy ("foo" :> Get '[JSON] FaultyRoundtrip)) `shouldBe`
-        (Proxy :: Proxy '[FaultyRoundtrip])
+        [faultyRoundtripRep]
 
     it "traverses :<|>" $ do
       let api :: Proxy (Get '[JSON] FaultyRoundtrip :<|> Get '[JSON] Bool)
           api = Proxy
-      usedTypes api `shouldBe` (Proxy :: Proxy [FaultyRoundtrip, Bool])
+      usedTypes api `shouldBe` [faultyRoundtripRep, boolRep]
 
 type ReqBodyFailApi =
   ReqBody '[JSON] FaultyRoundtrip :> Get '[JSON] Bool
@@ -50,3 +51,9 @@ type GetFailApi =
 
 getFailApi :: Proxy GetFailApi
 getFailApi = Proxy
+
+faultyRoundtripRep :: TypeRep
+faultyRoundtripRep = typeRep (Proxy :: Proxy FaultyRoundtrip)
+
+boolRep :: TypeRep
+boolRep = typeRep (Proxy :: Proxy Bool)
