d594f381e082eb8f2bc7f7dd4eaae81e66397398
Sönke Hahn <soenkehahn@gmail.com>
2016-07-05 20:00:13 +0800

    Rename type-classes

--- src/Servant/Aeson/Internal.hs
+++ src/Servant/Aeson/Internal.hs
@@ -26,55 +26,55 @@ import           Test.Aeson.Internal.RoundtripSpecs
 -- in a [servant](http://haskell-servant.readthedocs.org/) api.
 --
 -- See also 'Test.Aeson.RoundtripSpecs.roundtripSpecs'.
-apiRoundtripSpecs :: (HasRoundtripSpecs api) => Proxy api -> Spec
+apiRoundtripSpecs :: (HasGenericSpecs api) => Proxy api -> Spec
 apiRoundtripSpecs = sequence_ . map roundtrip . mkRoundtripSpecs
 
-apiGoldenSpecs :: HasRoundtripSpecs api => Proxy api -> Spec
+apiGoldenSpecs :: HasGenericSpecs api => Proxy api -> Spec
 apiGoldenSpecs proxy = sequence_ $ map golden $ mkRoundtripSpecs proxy
 
-apiSpecs :: (HasRoundtripSpecs api) => Proxy api -> Spec
+apiSpecs :: (HasGenericSpecs api) => Proxy api -> Spec
 apiSpecs proxy = sequence_ $ map (\ ts -> roundtrip ts >> golden ts) $ mkRoundtripSpecs proxy
 
 -- | Allows to retrieve a list of all used types in a
 -- [servant](http://haskell-servant.readthedocs.org/) api as 'TypeRep's.
-usedTypes :: (HasRoundtripSpecs api) => Proxy api -> [TypeRep]
+usedTypes :: (HasGenericSpecs api) => Proxy api -> [TypeRep]
 usedTypes = map typ . mkRoundtripSpecs
 
-mkRoundtripSpecs :: (HasRoundtripSpecs api) => Proxy api -> [TypeSpec]
+mkRoundtripSpecs :: (HasGenericSpecs api) => Proxy api -> [TypeSpec]
 mkRoundtripSpecs = normalize . collectRoundtripSpecs
   where
     normalize = nubBy ((==) `on` typ) . sortBy (compare `on` (show . typ))
 
-class HasRoundtripSpecs api where
+class HasGenericSpecs api where
   collectRoundtripSpecs :: Proxy api -> [TypeSpec]
 
-instance (HasRoundtripSpecs a, HasRoundtripSpecs b) => HasRoundtripSpecs (a :<|> b) where
+instance (HasGenericSpecs a, HasGenericSpecs b) => HasGenericSpecs (a :<|> b) where
   collectRoundtripSpecs Proxy =
     collectRoundtripSpecs (Proxy :: Proxy a) ++
     collectRoundtripSpecs (Proxy :: Proxy b)
 
-instance (MkSpec response) =>
-  HasRoundtripSpecs (Get contentTypes response) where
+instance (MkTypeSpecs response) =>
+  HasGenericSpecs (Get contentTypes response) where
 
   collectRoundtripSpecs Proxy = do
-    mkSpec (Proxy :: Proxy response)
+    mkTypeSpecs (Proxy :: Proxy response)
 
-instance (MkSpec response) =>
-  HasRoundtripSpecs (Post contentTypes response) where
+instance (MkTypeSpecs response) =>
+  HasGenericSpecs (Post contentTypes response) where
 
-  collectRoundtripSpecs Proxy = mkSpec (Proxy :: Proxy response)
+  collectRoundtripSpecs Proxy = mkTypeSpecs (Proxy :: Proxy response)
 
-instance (MkSpec body, HasRoundtripSpecs api) =>
-  HasRoundtripSpecs (ReqBody contentTypes body :> api) where
+instance (MkTypeSpecs body, HasGenericSpecs api) =>
+  HasGenericSpecs (ReqBody contentTypes body :> api) where
 
   collectRoundtripSpecs Proxy =
-    mkSpec (Proxy :: Proxy body) ++
+    mkTypeSpecs (Proxy :: Proxy body) ++
     collectRoundtripSpecs (Proxy :: Proxy api)
 
-instance HasRoundtripSpecs api => HasRoundtripSpecs ((path :: Symbol) :> api) where
+instance HasGenericSpecs api => HasGenericSpecs ((path :: Symbol) :> api) where
   collectRoundtripSpecs Proxy = collectRoundtripSpecs (Proxy :: Proxy api)
 
-instance HasRoundtripSpecs api => HasRoundtripSpecs (MatrixParam name a :> api) where
+instance HasGenericSpecs api => HasGenericSpecs (MatrixParam name a :> api) where
   collectRoundtripSpecs Proxy = collectRoundtripSpecs (Proxy :: Proxy api)
 
 data TypeSpec
@@ -84,14 +84,14 @@ data TypeSpec
     golden :: Spec
   }
 
--- 'mkSpec' has to be implemented as a method of a separate class, because we
+-- 'mkTypeSpecs' has to be implemented as a method of a separate class, because we
 -- want to be able to have a specialized implementation for lists.
-class MkSpec a where
-  mkSpec :: Proxy a -> [TypeSpec]
+class MkTypeSpecs a where
+  mkTypeSpecs :: Proxy a -> [TypeSpec]
 
-instance (Typeable a, Eq a, Show a, Arbitrary a, ToJSON a, FromJSON a) => MkSpec a where
+instance (Typeable a, Eq a, Show a, Arbitrary a, ToJSON a, FromJSON a) => MkTypeSpecs a where
 
-  mkSpec proxy = pure $
+  mkTypeSpecs proxy = pure $
     TypeSpec {
       typ = typeRep proxy,
       roundtrip = roundtripSpecs proxy,
@@ -103,9 +103,9 @@ instance (Typeable a, Eq a, Show a, Arbitrary a, ToJSON a, FromJSON a) => MkSpec
 -- test suites immensely.)
 instance {-# OVERLAPPING #-}
   (Eq a, Show a, Typeable a, Arbitrary a, ToJSON a, FromJSON a) =>
-  MkSpec [a] where
+  MkTypeSpecs [a] where
 
-  mkSpec Proxy = pure $
+  mkTypeSpecs Proxy = pure $
     TypeSpec {
       typ = typeRep proxy,
       roundtrip = genericAesonRoundtripWithNote proxy (Just note),
